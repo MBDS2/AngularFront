700d656f6e9b104096e7c418e09f5acdc2d7e8cc
karl011m <k.boue01@gmail.com>
2024-06-02 04:42:43 +0000

    micro service

--- src/app/assignment/utils/utils.ts
+++ src/app/assignment/utils/utils.ts
@@ -1,4 +1,4 @@
 // export const URL: string = "https://api-backend-xe27.onrender.com/api";
-export const URL: string= "https://backend-angular-a5t5.onrender.com/api";
 // export const URL: string = "http://localhost:8010/api";
+export const URL: string= "https://backend-angular-a5t5.onrender.com/api/assignments";
 
