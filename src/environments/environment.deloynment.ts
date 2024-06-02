939ddb211c9e09ffa2b7f033d9a2aba432a7909e
karl011m <k.boue01@gmail.com>
2024-06-02 07:55:00 +0000

    configs of front-back

--- src/environments/environment.deloynment.ts
+++ src/environments/environment.deloynment.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://backend-angular-a5t5.onrender.com', // Modifier cette URL
+  production: true,
+  apiUrl: 'https://backend-angular-a5t5.onrender.com/api', // Modifier cette URL
   envVar: {
-    endpointUrl: 'https://backend-angular-a5t5.onrender.com',
+    endpointUrl: 'https://backend-angular-a5t5.onrender.com/api',
     LOG_LEVEL: 'debug',
     verison: 0
   }
