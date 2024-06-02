668e0db1e88fce95d918d46ae4177fbe054fcaea
karl011m <k.boue01@gmail.com>
2024-06-02 07:35:26 +0000

    configs of front-back

--- src/environments/environment.deloynment.ts
+++ src/environments/environment.deloynment.ts
@@ -1,10 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8015/api',
+  apiUrl: 'https://backend-angular-a5t5.onrender.com/api', // Modifier cette URL
   envVar: {
-    /** * Add environment variables you want to retrieve from process
-     * PORT:4200,
-     * VAR_NAME: defaultValue */
     endpointUrl: 'https://backend-angular-a5t5.onrender.com/api',
     LOG_LEVEL: 'debug',
     verison: 0
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-   apiUrl: 'http://localhost:8010/api'
+  apiUrl: 'https://backend-angular-a5t5.onrender.com/api' // Modifier cette URL
 };
