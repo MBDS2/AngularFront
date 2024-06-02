ac95664be47887308909e40a927149b10909524e
karl011m <k.boue01@gmail.com>
2024-06-02 06:56:17 +0000

    configs of front-back

--- src/app/assignment/utils/utils.ts
+++ src/app/assignment/utils/utils.ts
@@ -1,6 +1,6 @@
 // export const URL: string = "https://api-backend-xe27.onrender.com/api";
-// export const URL: string = "http://localhost:8010/api";
+ export const URL: string = "http://localhost:8010/api";
 // export const URL: string= "https://backend-angular-a5t5.onrender.com/api/assignments";
 
 import { environment } from "src/environments/environment";
-export const URL: string = environment.apiUrl;
+// export const URL: string = environment.apiUrl;
